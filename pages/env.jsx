13d629684be4e356f7d94a8af759ef56c7cdbb86
Agus Rivero Ibanez <agusriv@amazon.es>
2023-05-25 13:11:37 +0200

    adding ssr

---
+++ pages/env.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PageLayout from "@/components/PageLayout";
+import styles from "@/styles/Home.module.css";
+
+export default function Env({ env }) {
+  return (
+    <PageLayout title="News App - Env Varibales" header="Env Varibales">
+      <section className={styles.container}>
+        <h1>{env}</h1>
+      </section>
+    </PageLayout>
+  );
+}
+
+export async function getServerSideProps() {
+  const env = process.env.NEXT_PUBLIC_ENV_VARIABLE;
+  console.log(env);
+  return {
+    props: {
+      env,
+    },
+  };
+}

44c117ac03f79c57b06a4b039eeecd99eb66e636
Agus Rivero Ibanez <agusriv@amazon.es>
2023-05-30 12:54:33 +0200

    adding SSR

--- pages/users.jsx
+++ pages/users.jsx
@@ -1,12 +1,25 @@
 import styles from "@/styles/Home.module.css";
 import PageLayout from "@/components/PageLayout";
+import Users from "@/components/Users";
 
 export default function About({ users }) {
   return (
     <PageLayout title="News App - Articles" header="Articles">
       <section className={styles.container}>
         <h1>Users section</h1>
+        <Users users={users} />
       </section>
     </PageLayout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
 }
